45a07b75bc9f7c854a290b9cc132f724ca355c17
Birgit Kratz <mail@birgitkratz.de>
2021-08-27 01:36:53 +0200

    initial commit

---
+++ bookshelf-server/src/test/java/de/birgitkratz/bookshelfserver/BookshelfServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.birgitkratz.bookshelfserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookshelfServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
