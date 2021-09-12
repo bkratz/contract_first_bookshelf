eec44869e3d7dbce397bdd207d69fe05c4e815a3
Birgit Kratz <mail@birgitkratz.de>
2021-09-12 19:48:45 +0200

    add fetching book by isbn, refactoring

--- bookshelf-client/src/main/java/de/birgitkratz/bookshelf/client/BookshelfClient.java
+++ bookshelf-client/src/main/java/de/birgitkratz/bookshelf/client/BookshelfClient.java
@@ -22,8 +22,7 @@ public class BookshelfClient {
         bookshelfClient.initApi();
 
         try {
-            var books = bookshelfClient.fetchAllBooks();
-            LOGGER.info("Received {} books from server", books.size());
+            bookshelfClient.fetchAllBooks();
 
             final var httpUndRest = new Book()
                     .isbn("9783898647328")
@@ -38,8 +37,11 @@ public class BookshelfClient {
             bookshelfClient.addBook(httpUndRest);
             bookshelfClient.addBook(refactoring);
 
-            books = bookshelfClient.fetchAllBooks();
-            LOGGER.info("Received {} books from server", books.size());
+            bookshelfClient.fetchAllBooks();
+
+            bookshelfClient.fetchByIsbn("1234567890123");
+            bookshelfClient.fetchByIsbn("9780134757599");
+
         } catch (ApiException e) {
             LOGGER.error("An ApiException occurred.", e);
         }
@@ -48,17 +50,32 @@ public class BookshelfClient {
     private void initApi() {
         ApiClient apiClient = new ApiClient();
         apiClient.setBasePath("http://localhost:8080");
-        api = new DefaultApi(new ApiClient());
-        api.setApiClient(apiClient);
+        api = new DefaultApi(apiClient);
     }
 
-    public List<Book> fetchAllBooks() throws ApiException {
+    List<Book> fetchAllBooks() throws ApiException {
         LOGGER.info("Trying to fetch books from bookshelf server");
-        return api.fetchAllBooks();
+        final var books = api.fetchAllBooks();
+        LOGGER.info("Received {} books from server", books != null ? books.size() : 0);
+        return books;
     }
 
-    public Book addBook(Book book) throws ApiException {
+    Book addBook(Book book) throws ApiException {
         LOGGER.info("Trying to add books with isbn {} to bookshelf", book.getIsbn());
         return api.addBook(book);
+    }
+
+    Book fetchByIsbn(String isbn) throws ApiException {
+        LOGGER.info("Trying to fetch book for isbn {} from bookshelf server", isbn);
+        final var bookApiResponse = api.fetchByIsbnWithHttpInfo(isbn);
+        if (bookApiResponse.getStatusCode() == 200) {
+            var bookByIsbn = bookApiResponse.getData();
+            LOGGER.info("Received book with title '{}' from bookshelf server", bookByIsbn.getTitle());
+            return bookByIsbn;
+        }
+        if (bookApiResponse.getStatusCode() == 204) {
+            LOGGER.info("No book for isbn {} in booshelf", isbn);
+        }
+        return null;
     }
 }
--- bookshelf-server/src/main/java/de/birgitkratz/bookshelfserver/BookshelfController.java
+++ bookshelf-server/src/main/java/de/birgitkratz/bookshelfserver/BookshelfController.java
@@ -12,7 +12,6 @@ import de.birgitkratz.bookshelf.model.Book;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,6 +34,24 @@ public class BookshelfController implements BooksApi {
     public ResponseEntity<List<Book>> fetchAllBooks() {
         LOGGER.info("Received request to fetch all books");
         final var books = new ArrayList<>(bookshelf.values());
+        if (books.isEmpty()) {
+            LOGGER.info("No books in shelf");
+            return ResponseEntity.noContent().build();
+        }
+        LOGGER.info("Returning {} books", books.size());
         return ResponseEntity.ok(books);
+    }
+
+    @Override
+    public ResponseEntity<Book> fetchByIsbn(final String isbn) {
+        LOGGER.info("Received request to fetch book by isbn {}", isbn);
+        final var book = bookshelf.get(isbn);
+
+        if (book == null) {
+            LOGGER.info("No book found");
+            return ResponseEntity.noContent().build();
+        }
+        LOGGER.info("Found it!");
+        return ResponseEntity.ok(book);
     }
 }
